faeec73ee313b58a52290fed4774fa007e39c832
JGuo <jason.guo2560@gmail.com>
2022-09-28 19:14:08 -0400

    included routes for basic pages, started work on side navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
 import PageHeader from "./components/PageHeader";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <PageHeader />
-    </div>
+      <>
+          <PageHeader />
+          <Routes>
+              <Route path="/" element={<div>Home</div>} />
+              <Route path="/files" element={<div>Files</div>} />
+              <Route path="/myfiles" element={<div>MyFiles</div>} />
+              <Route path="/sharedfiles" element={<div>SharedFiles</div>} />
+              <Route path="/folder/:folderId" element={<div>OpenFolder</div>} />
+              <Route path="/file/:fileId" element={<div>OpenFile</div>} />
+              <Route path="/filesnapshot" element={<div>FileSnapshot</div>} />
+              <Route path="/groupsnapshot" element={<div>GroupSnapshot</div>} />
+          </Routes>
+      </>
   );
 }
 
---
+++ src/components/PageSideBar.jsx
@@ -0,0 +1,8 @@
+import React, {useState} from 'react';
+
+export default function PageSideBar() {
+    const [openPage, setOpenPage] = useState("");
+
+
+    return null;
+}
