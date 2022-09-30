17feca57a0ea96b95fd43f68f4ddb141abb4d7f2
JGuo <jason.guo2560@gmail.com>
2022-09-29 21:11:41 -0400

    updated styling on login buttons, included Emmirhan work on login to Google

---
+++ src/components/pages/AllFiles/AllFilesPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PageSideBar from "../../common-page-components/PageSidebar/PageSideBar";
+import "../index.css";
+
+export default function AllFilesPage() {
+    return (
+        <div className="page-container">
+            <PageSideBar />
+            <div className="page-content">
+                <div className="page-content-header"></div>
+
+            </div>
+        </div>
+    );
+}
