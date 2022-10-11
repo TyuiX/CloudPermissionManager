c4211d9b5b51766020f8431656f2d12072f860f0
JGuo <jason.guo2560@gmail.com>
2022-10-10 20:03:47 -0400

    added function link google to user profile

--- src/components/common-page-components/PageSidebar/LinkGoogleLink/LinkGoogleLink.jsx
+++ src/components/common-page-components/PageSidebar/LinkGoogleLink/LinkGoogleLink.jsx
@@ -1,30 +1,22 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {GoogleLogin} from "react-google-login";
 import googleAuth from "../../../../utils/GoogleAuth";
+import {UserContext} from "../../../../utils/context/UserContext";
+import {gapi} from "gapi-script";
 
 export default function LinkGoogleLink() {
+    const {user, setGoogleAcc} = useContext(UserContext)
 
     const onSuccess = async (res) => {
         console.log(res)
         console.log("login success: ", res.profileObj);
 
-        // refreshTokenSetup(res)
+        setGoogleAcc(user.email, gapi.auth2.getAuthInstance().currentUser.get().getBasicProfile().cu)
     }
 
     const onFailure = (res) => {
         console.log("login failed: " + res);
     }
-
-    // const refreshTokenSetup = (res) => {
-    //     let refreshTiming = (res.tokenObj.expires_in || 3600 - 5 * 60) * 1000
-    //
-    //     const refreshToken = async () => {
-    //         const newAuthRes = await res.reloadAuthResponse();
-    //         refreshTiming = (newAuthRes.tokenObj.expires_in || 3600 - 5 * 60) * 1000
-    //         setTimeout(refreshToken, refreshTiming)
-    //     }
-    //     setTimeout(refreshToken, refreshTiming)
-    // }
 
     return (
         <div>
@@ -33,7 +25,6 @@ export default function LinkGoogleLink() {
                 onSuccess={onSuccess}
                 onFailure={onFailure}
                 cookiePolicy={"single_host_origin"}
-                isSignedIn={true}
                 buttonText="Link Google account"
             />
         </div>
