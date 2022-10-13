d962f89771e0a0a613a357e84fe6e45210fd4c22
varunchotalia <varunvinay.chotalia@stonybrook.edu>
2022-10-13 18:41:37 -0400

    one drive authentication

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import PageHeader from "./components/common-page-components/PageHeader/PageHeader";
+import {PageLayout} from "./components/pages/OneDriveAuth/PageLayout";
 import { Route, Routes } from "react-router-dom";
 import MyFiles from "./components/pages/MyFiles/MyFiles";
-import {PageLayout} from "./components/onedriveauth/PageLayout";
+import MySnapshots from "./components/pages/Snapshot/MySnapshots";
 import LinkGoogleLink from "./components/common-page-components/PageSidebar/LinkGoogleLink/LinkGoogleLink";
-import React, {useContext} from "react";
+import React, {useContext, useState, useEffect} from "react";
 import SignUp from "./components/pages/SignUp/SignUp";
 import Login from "./components/pages/Login/Login";
 import SharedFiles from "./components/pages/SharedFiles/SharedFiles";
@@ -12,28 +13,55 @@ import Home from "./components/pages/Home/Home";
 import * as event from "./components/functions/events"
 import {UserContext} from "./utils/context/UserContext";
 import LoadingScreen from "./components/common-page-components/LoadingScreen/LoadingScreen";
+import { getSnapshots, getUserProfile } from './api/ShareManagerAPI';
 
 function App() {
     const {isLoading} = useContext(UserContext)
     console.log(isLoading)
 
+    const [snapshots, setSnap] = useState([]);
+
+    const getProfile = async () => {
+        let user = localStorage.getItem('user');
+        const parsedUser = JSON.parse(user)
+        console.log(parsedUser);
+        return await getUserProfile({email: parsedUser.email});
+    }
+
+    const getSnap = async (msg) => {
+        return await getSnapshots(msg);
+    }
+
+    useEffect(() => {
+        getProfile().then(res => {
+            getSnap(res.data.snapshot).then(res2 => {
+                setSnap(res2);
+            })
+        });
+        // event.handleGetFile();
+        console.log("in use effect");
+        console.log(snapshots);
+    }, []);
+
+    console.log(snapshots)
     if (isLoading) {
         return <LoadingScreen />
     }
 
   return (
       <>
-          <PageHeader  />
+          <PageHeader snapshots={snapshots} />
           <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/login" element={<Login />} />
-              <Route path="/login/one" element={<PageLayout />} />
               <Route path="/login/google" element={<LinkGoogleLink />} />
+              <Route path="/login/one" element={<PageLayout />} />
               <Route path="/signup" element={<SignUp />} />
               <Route path="/files" element={<MyFiles />} />
               <Route path="/sharedfiles" element={<SharedFiles />} />
               <Route path="/folder/:folderId" element={<div>OpenFolder</div>} />
-              <Route path="/filesnapshot" element={<div >FileSnapshot <button onClick={event.handleGetFile}>click me </button></div>} />
+              {/* <Route path="/filesnapshot" element={<div >FileSnapshot <button onClick={event.handleGetFile}>click me </button></div>} /> */}
+              <Route path="/filesnapshot" element={<MySnapshots newSnap={event.handleGetFile} snapshot={snapshots}/>} />
               <Route path="/groupsnapshot" element={<div>GroupSnapshot</div>} />
           </Routes>
       </>
