4e1c8c465776e69adf1e6db6f6e0387b017d3632
JGuo <jason.guo2560@gmail.com>
2022-11-06 16:06:44 -0500

    accounted for displaying group and anyone with link perm for files, added option for group and anyone with link perms for singular files

--- src/components/common-page-components/FileInfoSideBar/PermissionsCell/PermissionsCell.jsx
+++ src/components/common-page-components/FileInfoSideBar/PermissionsCell/PermissionsCell.jsx
@@ -3,14 +3,24 @@ import "./PermissionsCell.css";
 
 export default function PermissionsCell(props) {
     const {permInfo} = props;
-    const {displayName, role, emailAddress} = permInfo;
+    const {displayName, role, emailAddress, type} = permInfo;
 
     return (
         <div className="permission-cell">
             <div className="permission-info">
-                <div className="permission-name">{displayName}</div>
-                <div className="permission-info">Email: {emailAddress}</div>
-                <div className="permission-info">Role: {role}</div>
+                {/*check if perm is for specific user/group or anyone*/}
+                {type !== "anyone" ?
+                    <>
+                        <div className="permission-name">{displayName}</div>
+                        <div className="permission-info">Email: {emailAddress}</div>
+                        <div className="permission-info">Role: {role}</div>
+                    </>
+                    :
+                    <>
+                        <div className="permission-name">Anyone with Link</div>
+                        <div className="permission-info">Role: {role}</div>
+                    </>
+                }
             </div>
         </div>
     );
